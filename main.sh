93ebc3086a9b381c49aabae4580d5710ac9dd700
nazri <ayiehere@gmail.com>
2013-02-22 10:27:08 +0800

    main.sh - only source healthy symlink

--- main.sh
+++ main.sh
@@ -1,6 +1,10 @@
 RCHOME=`dirname $0`
 for rcfile in $RCHOME/*.rc
 do
-  source $rcfile
+  if [ -f $rcfile ]; then
+    source $rcfile
+  elif [ -h $rcfile && readlink -e $rcfile ]; then
+    source $rcfile
+  fi
 done
 echo $PWD > /dev/shm/$$.cwd
