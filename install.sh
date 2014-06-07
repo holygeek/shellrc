569180b34805b2da6d0c595f2aae815358a1f56a
Nazri Ramliy <ayiehere@gmail.com>
2014-06-07 12:13:57 +0800

    Add elinks.conf

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 ln -s `pwd`/tmux.conf ~/.tmux.conf
 ln -s `pwd`/inputrc ~/.inputrc
 ln -s `pwd`/keynavrc ~/.keynavrc
+
+elinks_conf=$HOME/.elinks/elinks.conf
+if [ -w $elinks_conf ]; then
+  if ! grep -q "source \".*.shell/elinks\.conf\"" $elinks_conf; then
+    echo "source \"$HOME/.shell/elinks.conf\"" >> $elinks_conf
+  fi
+else
+    echo "source \"$HOME/.shell/elinks.conf\"" >> $elinks_conf
+fi
