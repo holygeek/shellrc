5a8c891b369a45000c9c7c4eb3bb4e50e833397a
Nazri Ramliy <ayiehere@gmail.com>
2014-10-30 08:45:31 +0800

    Add offlineimaprc

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 ln -s `pwd`/tmux.conf ~/.tmux.conf
 ln -s `pwd`/inputrc ~/.inputrc
 ln -s `pwd`/keynavrc ~/.keynavrc
+ln -s `pwd`/offlineimaprc ~/.offlineimaprc
 
 elinks_conf=$HOME/.elinks/elinks.conf
 if [ -w $elinks_conf ]; then
